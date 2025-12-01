33cfe7918c9dcce924802b84bba79e23ca8e9d57
ahmed-khaled04 <ahmed1blackops8@gmail.com>
2025-12-01 23:14:56 +0200

    Added Ready And GUI And handling snapshots

--- protocol.py
+++ protocol.py
@@ -17,7 +17,7 @@ from enum import IntEnum
 # ---------------------------------------------------------
 
 PROTOCOL_ID = b"GSCP"   # 4 bytes (Grid Sync Clash)
-VERSION = 1            # 1 byte protocol version
+VERSION = 2           # 1 byte protocol version
 
 # ---------------------------------------------------------
 # Message Types
@@ -28,8 +28,9 @@ class MsgType(IntEnum):
     JOIN_ACK = 1      # Server → Client
     EVENT = 2         # Client → Server
     SNAPSHOT = 3      # Server → Client
-    HEARTBEAT = 4
-    ERROR = 5
+    READY = 4
+    HEARTBEAT = 5
+    ERROR = 6
 
 # ---------------------------------------------------------
 # Header Structure
@@ -69,8 +70,12 @@ EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 #   player_id    2 bytes
 #   grid_size    1 byte
 #   tick_rate    1 byte
+#   color_r      1 byte
+#   color_g      1 byte
+#   color_b      1 byte
 
-JOIN_ACK_FORMAT = "!H B B"
+
+JOIN_ACK_FORMAT = "!H B B B B B"
 JOIN_ACK_SIZE = struct.calcsize(JOIN_ACK_FORMAT)
 
 # ---------------------------------------------------------
--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-import socket
+import threading
 import time
+import socket
 import struct
+
 
 from protocol import (
     HEADER_FORMAT, HEADER_SIZE, MsgType,
     PROTOCOL_ID, VERSION,
     JOIN_ACK_FORMAT, JOIN_ACK_SIZE,
-    GRID_SIZE
+    GRID_SIZE,
+    SNAPSHOT_SIZE , 
+    EventType, EVENT_FORMAT, EVENT_SIZE
 )
+
+PLAYER_COLORS = [
+    (255,0,0),    
+    (0,255,0),    
+    (0,0,255),    
+    (255,255,0),  
+    (255,0,255),  
+    (0,255,255),  
+]
+
+def assign_color(player_id):
+    return PLAYER_COLORS[player_id % len(PLAYER_COLORS)]
 
 
 # Server settings
@@ -32,12 +48,12 @@ addr_to_player = {}
 # Game state: 20x20 grid, each byte = cell owner (0 = unclaimed)
 grid = [0] * (GRID_SIZE * GRID_SIZE)
 
-clients = set()
+connected_players = {}
 
 print(f"[SERVER] Running snapshot broadcaster on {ADDR}")
 
 TICK_RATE = 20          # 20 Hz → every 50 ms
-TICK_INTERVAL = 1 / TICK_RATE
+TICK_INTERVAL = 1.0 / TICK_RATE
 
 def pack_header(msg_type, snapshot_id, seq_num, timestamp_ms, payload_len):
     return struct.pack(
@@ -51,6 +67,41 @@ def pack_header(msg_type, snapshot_id, seq_num, timestamp_ms, payload_len):
         payload_len
     )
 
+
+def snapshot_sender():
+    global snapshot_id
+
+    print("[SERVER] Snapshot thread started ...")
+
+    while True :
+        now_ms = int(time.time() * 1000)
+
+        payload = bytes(grid)
+
+        header = pack_header(
+            MsgType.SNAPSHOT,
+            snapshot_id,
+            seq_num,
+            now_ms,
+            SNAPSHOT_SIZE
+        )
+
+        packet = header + payload
+
+        for player_addr in connected_players.values():
+            server.sendto(packet , player_addr)
+
+        snapshot_id += 1
+
+        time.sleep(TICK_INTERVAL)
+
+
+
+snapshot_thread = threading.Thread(target=snapshot_sender , daemon=True)
+snapshot_thread.start()
+
+
+
 while True:
     try:
         # Receive data from client
@@ -59,11 +110,8 @@ while True:
         if len(data) < HEADER_SIZE:
             print(f"[WARN] Short packet from {client_addr}, ignoring")
             continue
-
-        print("[SERVER] Raw packet length:", len(data))
-        print("[SERVER] Raw packet bytes:", data)
         
-        prot_id, ver, msg_type_val, snapshot_id, seq_num, ts, payload_len = \
+        prot_id, ver, msg_type_val, recv_snapshot_id, recv_seq_num, ts, payload_len = \
             struct.unpack(HEADER_FORMAT, data[:HEADER_SIZE])
         
         msg_type = MsgType(msg_type_val)
@@ -83,7 +131,9 @@ while True:
             
             print(f"[SERVER] JOIN from {client_addr} -> assigned player_id {player_id}")
 
-            payload = struct.pack(JOIN_ACK_FORMAT, player_id, GRID_SIZE, TICK_RATE)
+            color_r, color_g, color_b = assign_color(player_id)
+
+            payload = struct.pack(JOIN_ACK_FORMAT, player_id, GRID_SIZE, TICK_RATE , color_r, color_g, color_b)
 
             # Build response header
             timestamp_ms = int(time.time() * 1000)
@@ -100,35 +150,37 @@ while True:
             server.sendto(header + payload, client_addr)
             print(f"[SERVER] Sent JOIN_ACK to {client_addr}")
 
-        #snapshot logic
-        start = time.time()
+        elif msg_type == MsgType.READY:
+            player_id = addr_to_player.get(client_addr)
 
-        snapshot_id += 1
-        seq_num += 1
-        timestamp_ms = int(time.time() * 1000)
+            if not player_id:
+                print("[SERVER] READY from unknown client ignoring" , client_addr)
+                continue
+            connected_players[player_id] = client_addr
+            print("[SERVER] Player added to snapshot list with id: " , player_id)
+            continue
+        
+        elif msg_type == MsgType.EVENT:
 
-        # Build header
-        payload = bytes(grid)  # full snapshot: 400 bytes
-        payload_len = len(payload)
+            if len(data) < HEADER_SIZE + EVENT_SIZE:
+                print("[SERVER] Invalid Event size")
+                continue
+            
+            payload = data[HEADER_SIZE : HEADER_SIZE + EVENT_SIZE]
 
-        header = pack_header(
-                MsgType.SNAPSHOT,
-                snapshot_id,
-                seq_num,
-                timestamp_ms,
-                payload_len
-            )
+            player_id, client_seq, event_type, cell_index , client_ts = \
+                struct.unpack(EVENT_FORMAT , payload)
+            
+            print(f"[SERVER] Event from player {player_id}: type={event_type}, cell={cell_index}")
 
-        packet = header + payload
+            row = cell_index // GRID_SIZE
+            col = cell_index % GRID_SIZE
 
-        # Send snapshot to all connected clients
-        for c in clients:
-            server.sendto(packet, c)
+            if event_type == EventType.CLICK and grid[row * GRID_SIZE + col] == 0:
+                grid[row * GRID_SIZE + col] = player_id
+            
 
-        # Sleep until next tick
-        elapsed = time.time() - start
-        delay = max(0, TICK_INTERVAL - elapsed)
-        time.sleep(delay)
+
 
     except KeyboardInterrupt:
         print("\n[SERVER] Shutting down...")
